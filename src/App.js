d6da7e1c56834f0cdef303281315fd8e8fb9859d
Valeriy-dev-js <vorobvdev@gmail.com>
2021-05-24 16:48:23 +0300

    add some paginate logic

--- src/App.js
+++ src/App.js
@@ -12,62 +12,70 @@ function App() {
   { id: 3, title: '???', completed: true, date: 1 }];
 
   //State
-  const [todos, setTodos] = useState(data)
-  const [sorterFilter, setSorterFilter] = useState({ sorterType: true, filterType: 'All' })
-
+  const [todos, setTodos] = useState(data);
+  const [sorterFilter, setSorterFilter] = useState({ sorterType: true, filterType: 'All' });
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postPerPage] = useState(5);
+  
+  
 
 
   //Action functions
   const handleSubmit = (todo) => {
     if (todo !== '') {
       setTodos(prev => [{ id: Date.now(), title: todo.trim(), completed: false, date: Date.now() }, ...prev])
-    }
+    };
   };
 
   const handleDelete = (id) => {
-    setTodos(prev => prev.filter(task => task.id !== id))
+    setTodos(prev => prev.filter(task => task.id !== id));
   };
 
   const handleCheck = (id) => {
-    const newTodos = [...todos]
-    const index = newTodos.findIndex(todo => todo.id === id)
-    newTodos[index].completed = !newTodos[index].completed
-    setTodos(newTodos)
-  }
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(todo => todo.id === id);
+    newTodos[index].completed = !newTodos[index].completed;
+    setTodos(newTodos);
+  };
 
   const handleTodoChange = (id, inputValue) => {
-    const newTodos = [...todos]
-    const index = newTodos.findIndex(todo => todo.id === id)
-    newTodos[index].title = inputValue
-    setTodos(newTodos)
-  }
+    const newTodos = [...todos];
+    const index = newTodos.findIndex(todo => todo.id === id);
+    newTodos[index].title = inputValue;
+    setTodos(newTodos);
+  };
 
   //Sorting and Filtering logic
-  const resultTodos = useMemo(() => {
-
-    const iteratingTodos = [...todos]
-    const { sorterType, filterType } = sorterFilter
+  const sortFiltTodos = useMemo(() => {
+    const iteratingTodos = [...todos];
+    const { sorterType, filterType } = sorterFilter;
     //Sorting todos by date
     const sortedTodos = iteratingTodos.sort((a, b) => {
       if (sorterType){
-        return b.date - a.date
+        return b.date - a.date;
       }
-      return a.date - b.date})
+      return a.date - b.date});
     //Filtering todos by completed
     const filteredTodos = sortedTodos.filter(item => {
       switch(filterType){
         case 'All':
           return item;
         case 'Done':
-          return item.completed === true
+          return item.completed === true;
         default:
           return item.completed === false;
       }
     })
+    return filteredTodos;
+  }, [todos, sorterFilter]);
 
-
-    return filteredTodos
-  }, [todos, sorterFilter])
+  const paginateTodos = useMemo(() => {
+    const indexOfLastPost = currentPage * postPerPage;
+    const indexOfFirstPost = indexOfLastPost - postPerPage;
+    const currentPosts = sortFiltTodos.slice(indexOfFirstPost, indexOfLastPost);
+    return currentPosts
+    console.log(JSON.stringify(currentPosts, null, 2));
+  },[currentPage, todos, sorterFilter]);
 
 
 
@@ -86,7 +94,7 @@ function App() {
         sorterFilter={sorterFilter}
         setSorterFilter={setSorterFilter} />
       <ToDoList
-        todos={resultTodos}
+        todos={paginateTodos}
         handleCheck={handleCheck}
         handleDelete={handleDelete}
         handleTodoChange={handleTodoChange}/>
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,9 +1,14 @@
 import { Button, ButtonGroup, Grid } from '@material-ui/core';
 import ArrowForwardIosIcon from '@material-ui/icons/ArrowForwardIos';
 import ArrowBackIosIcon from '@material-ui/icons/ArrowBackIos';
-import React from 'react';
+import React, { useRef } from 'react';
 
 export const Pagination = () => {
+    const pageNumbers = []
+    for (let i = 1; i <= Math.ceil(totalPosts / postsPerPage); i++) {
+        pageNumbers.push(i);
+      }
+
     return(
         <ButtonGroup fullWidth >
             <Button >
