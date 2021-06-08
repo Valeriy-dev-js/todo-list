9361668d79c9c63c52dfd0b1a0a4130ee5210719
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-08 12:19:06 +0300

    Change auth logic

--- src/components/Auth.js
+++ src/components/Auth.js
@@ -2,8 +2,9 @@ import { Button, ButtonGroup, Grid, TextField } from "@material-ui/core"
 import { useState } from "react"
 import axios from "../axiosConfig";
 
-export const Auth = ({ setIsLogin, signup}) => {
+export const Auth = ({ setIsLogin, signup }) => {
     const [user, setUser] = useState({ name: '', password: '' });
+    const [helperText, setHelperText] = useState('');
     const login = async () => {
         try {
             const res = await axios.post('/login', {
@@ -13,9 +14,10 @@ export const Auth = ({ setIsLogin, signup}) => {
             setUser({ name: '', password: '' })
             localStorage.setItem('token', res.data.token)
             setIsLogin(false)
-            
+
         } catch (err) {
-            console.log(err.response.data);
+            const message = err.response.data.message;
+            setHelperText(message);
         };
     };
     const signUp = async () => {
@@ -24,44 +26,58 @@ export const Auth = ({ setIsLogin, signup}) => {
                 'name': user.name,
                 'password': user.password
             });
-            login()
+            // login()
         } catch (err) {
-            console.log(err.response.data);
+            const message = err.response.data.message;
+            setHelperText(message);
         };
     };
+    const handleChange = (e) => {
+        const value = e.target.value;
+        setUser({ ...user, [e.target.name]: value });
+        setHelperText('');
+    }
     return (
-        <Grid
-            container
-            direction='column'
-            alignItems='center'>
-            <form>
+        <form onSubmit={e => e.preventDefault()}>
+            <Grid
+                container
+                direction='column'
+                alignItems='center'>
                 <TextField
-                    onChange={e => setUser({ ...user, name: e.target.value })}
+                    name='name'
+                    error={helperText !== ''}
+                    helperText={helperText}
+                    onChange={e => handleChange(e)}
                     value={user.name}
-                    label='User Name'
+                    label='Username'
+                    required
                     fullWidth
                     variant='outlined'
                     margin='normal' />
                 <TextField
-                    onChange={e => setUser({ ...user, password: e.target.value })}
+                    name='password'
+                    required
+                    onChange={e => handleChange(e)}
                     value={user.password}
                     label='Password'
                     fullWidth
                     variant='outlined'
                     margin='normal'
                     type='password' />
-            </form>
-            <ButtonGroup>
+
                 {signup
-                ?<Button
-                    onClick={() => login()}
-                    color='primary'
-                    variant='contained'>Login</Button>
-                :<Button
-                    onClick={() => signUp()}
-                    color='secondary'
-                    variant='contained'>sign up</Button>}
-            </ButtonGroup>
-        </Grid>
+                    ? <Button
+                        type='submit'
+                        onClick={() => login()}
+                        color='primary'
+                        variant='contained'>Login</Button>
+                    : <Button
+                        type='submit'
+                        onClick={() => signUp()}
+                        color='secondary'
+                        variant='contained'>sign up</Button>}
+
+            </Grid>
+        </form>
     )
 }
