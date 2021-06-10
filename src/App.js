3357cd05214d6c8879faa8f2e8fed338f77e7d8a
Valeriy-dev-js <vorobvdev@gmail.com>
2021-06-10 16:08:19 +0300

    Change checktoken logic

--- src/App.js
+++ src/App.js
@@ -1,22 +1,25 @@
-import { useCallback, useLayoutEffect} from 'react';
+import { useCallback, useLayoutEffect } from 'react';
 import './App.css';
 import { Todo } from './components/Todo';
 import { Auth } from './components/auth/Auth';
 import { Container } from '@material-ui/core';
 import { Header } from './components/Header';
 import { useDispatch, useSelector } from 'react-redux';
-import { selectIsAuth, toggleAuth } from './components/auth/authSlice'
+import { selectIsAuth, toggleAuth } from './components/auth/authSlice';
+import { decode } from 'jsonwebtoken';
 
 function App() {
     const isAuth = useSelector(selectIsAuth)
     const dispatch = useDispatch()
 
     const checkToken = useCallback(() => {
-        if (localStorage.token) dispatch(toggleAuth());
-    }, [dispatch]);
+        if (!localStorage.token) return
+        const data = decode(localStorage.token)?.exp
+        if (Date.now() > data) dispatch(toggleAuth());
+    }, [ dispatch ]);
     useLayoutEffect(() => {
         checkToken();
-    }, [checkToken]);
+    }, [ checkToken ]);
 
     return (
         <Container maxWidth='sm'>
